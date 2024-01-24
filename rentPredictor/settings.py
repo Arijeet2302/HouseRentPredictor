7832182929771bba62c516c37ffe4c4b4f2d479d
Arijeet Sinha <arijeetsinha2@gmail.com>
2024-01-25 01:05:17 +0530

    host updated

--- rentPredictor/settings.py
+++ rentPredictor/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-lozc#q56(*l038p(82c=_slx^3ajhniax=7s4v(5q(ue5mr68w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh','localhost']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh','localhost','127.0.0.1']
 
 
 # Application definition
