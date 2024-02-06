53e34c32bec9fa0b0ec72a7ff041f8e1b395e9b4
ArijeetInfomaticae <arijeetsinha2@gmail.com>
2024-02-07 02:23:28 +0530

    mail-classifier added

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,7 @@ from myapp import views
 
 urlpatterns = [
     path("", views.index, name="index"),  
+    path("mail", views.mail_checker, name="mail_checker"),
     path("predict", views.predict, name="predict"), 
+    path("mail_check", views.predict_mail, name="mail_check"), 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 import pandas as pd
 import joblib as jl
+import pickle
 
 # Create your views here.
 
 sc = jl.load('static/Standard_scaler.joblib')
 model = jl.load('static/House_Price_precdictor.joblib')
+mail_model = pickle.load(open('static/model.pkl', 'rb'))
+vector = pickle.load(open('static/vectorizer.pkl', 'rb'))
 
 def index(request):
     data = pd.read_csv('static/House_Rent_Dataset.csv')
@@ -49,3 +52,28 @@ def predict(request):
     pred = model.predict([df.iloc[0]])
 
     return render(request, 'index.html', {'pred': int(pred[0])})
+
+
+def mail_checker(request):
+
+    return render(request, 'mail.html', { 'mail': None })
+
+@csrf_exempt
+def predict_mail(request):
+
+    if request.method == "POST":
+        message = request.POST['mail']
+
+    processed_msg = vector.transform([message])
+
+    prediction = mail_model.predict(processed_msg)
+
+    pred_msg = "Not Spam"
+    print(prediction[0])
+    spam = False
+
+    if prediction[0] == 1: 
+        pred_msg = "Spam"
+        spam = True
+
+    return render(request, 'mail.html', {'mail': pred_msg, 'spam': spam })
